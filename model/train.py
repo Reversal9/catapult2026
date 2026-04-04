c8a227966763b8ef4c6e7b488596b18ff6c0c21c
root <root@agrn-pvnozha.localdomain>
2026-04-04 15:39:03 -0500

    bivonodesu

--- model/train.py
+++ model/train.py
@@ -17,16 +17,18 @@ import torch.nn.functional as F
 class Habakkuk(nn.Module):
     def __init__(self, input_size):
         super(Habakkuk, self).__init__()
-        self.fc1 = nn.Linear(input_size, 48)
-        self.fc2 = nn.Linear(48, 24)
-        self.fc3 = nn.Linear(24, 12)
-        self.fc4 = nn.Linear(12, 6)
+        self.fc1 = nn.Linear(input_size, 480)
+        self.fc2 = nn.Linear(480, 100)
+        self.fc3 = nn.Linear(100, 24)
+        self.do = nn.Dropout(p=0.2)
+        self.fc4 = nn.Linear(24, 6)
         self.fc5 = nn.Linear(6, 1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
+        #x = self.do(x)
         x = F.relu(self.fc4(x))
         x = self.fc5(x)
         return x
